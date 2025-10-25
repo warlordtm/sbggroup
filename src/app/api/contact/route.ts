5ecf4ad26bac4e4a9333a90f6da13ba034e1b2b9
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-10-25 21:53:18 +0100

    REMOVE backend

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,31 +1,29 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import dotenv from "dotenv";
-dotenv.config();
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 
-export const runtime = "nodejs";
+// export const runtime = "nodejs";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-console.log("Resend key:", process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-export async function POST(req: Request) {
-  try {
-    const { firstName, email, message, phone } = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const { firstName, email, message, phone } = await req.json();
 
-    await resend.emails.send({
-      from: "Portfolio Contact <onboarding@resend.dev>",
-      to: "bamisayegodwin7@gmail.com",
-      subject: `New message from ${firstName}`,
-      replyTo: email,
-      text: `Email: ${email}\n\nMessage: ${message}, My phone number is ${phone}`,
-    });
+//     await resend.emails.send({
+//       from: "Portfolio Contact <onboarding@resend.dev>",
+//       to: "bamisayegodwin7@gmail.com",
+//       subject: `New message from ${firstName}`,
+//       replyTo: email,
+//       text: `Email: ${email}\n\nMessage: ${message}, My phone number is ${phone}`,
+//     });
 
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ success: true });
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ success: false }, { status: 500 });
+//   }
+// }
