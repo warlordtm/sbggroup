f471f2792392e59a7e9a2dceeddc6efc14ef9f6a
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-10-25 21:35:31 +0100

    Update

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export const runtime = "nodejs";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+console.log("Resend key:", process.env.RESEND_API_KEY);
+
 
 export async function POST(req: Request) {
   try {
